6e2362c3ec8dcc59334dd5505354b66dbe848666
khaliunb <khaliun83@yahoo.com>
2021-04-05 14:26:03 +0800

    Initial Commit for the project by Khaliun.B 2021.04.05 14:25

---
+++ MovieLensAnalysisScripts.R
@@ -0,0 +1,8 @@
+#Project for HarvardX Data Science Professional Certificate
+#Start Date: 2021.04.05
+#Project is based on code provided by Machine Learning Course
+
+#Course: Capstone
+#Project: HarvardXCapstoneMovieLens_ProjectRepo.Rproj
+#Author: Khaliun.B
+
